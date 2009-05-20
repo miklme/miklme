7d8cef032db62a1e06e33c0d528520aaebd35602
Michael <xxmikl.mexx@gmail.com>
2009-05-20 23:05:48 +0800

    增加了RJS模板，具有了一定的视觉效果。
    增强意识形态导向，这就是意义

--- app/controllers/before_start_controller.rb
+++ app/controllers/before_start_controller.rb
@@ -2,5 +2,12 @@ class BeforeStartController < ApplicationController
   def about_three_body
     
   end
+  def michael
+  end
+  def us
+  end
+  def index
+    
+  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -38,6 +38,7 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing the them or commenting them out if you're using named routes and resources.
+  map.resources :before_start_resources
   map.resources :users
   map.resource :session
   map.connect ':controller/:action/:id'
