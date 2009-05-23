efd32a433908f605543c676f9528dc4e0c7447b9
Michael <xxmikl.mexx@gmail.com>
2009-05-23 23:07:23 +0800

    Successfully add function of restful authentication.
    clean,tidy

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 ActionController::Routing::Routes.draw do |map|
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+  map.login '/login', :controller => 'sessions', :action => 'new'
+  map.register '/register', :controller => 'users', :action => 'create'
+  map.signup '/signup', :controller => 'users', :action => 'new'
+  map.resources :users
+  map.resource :session
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -31,16 +38,13 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-   map.root :controller => "before_start"
+  #map.root :controller => "before_start"
 
   # See how all your routes lay out with "rake routes"
 
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing the them or commenting them out if you're using named routes and resources.
-  map.resources :before_start_resources
-  map.resources :users
-  map.resource :session
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
