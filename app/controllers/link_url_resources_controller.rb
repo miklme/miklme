6e225413670a82c297a3a72366161111464f6b33
Michael <xxmikl.mexx@gmail.com>
2009-07-25 18:44:20 +0800

    There seems to be a big big change...Rebuild the keywords system...Oh my god.

--- app/controllers/link_url_resources_controller.rb
+++ app/controllers/link_url_resources_controller.rb
@@ -15,7 +15,7 @@ class LinkUrlResourcesController < ApplicationController
   def create
     @link_url_resource=current_user.resources.build
     if !params[:resource][:title].blank? and !params[:resource][:keywords].blank? and !flash[:link_url].blank?
-      @link_url_resource.update_attributes(:resource_type => "link_url_resource",:title=>params[:title], :keywords=>params[:keywords],:link_url => flash[:link_url])
+      @link_url_resource.update_attributes(:type => "link_url_resource",:title=>params[:title], :keywords=>params[:keywords],:link_url => flash[:link_url])
       @link_url_resource.save
       render :update do |page|
         page.redirect_to user_path(current_user)
--- app/controllers/twitter_resources_controller.rb
+++ app/controllers/twitter_resources_controller.rb
@@ -1,4 +1,5 @@
 class TwitterResourcesController < ApplicationController
+  before_filter :find_user
   def new
   end
 
@@ -12,17 +13,21 @@ class TwitterResourcesController < ApplicationController
   end
 
   def create
-    if  params[:keywords] !~%r{[a-zA-z]+://[^\s]*}
-      @resource.save!
+    @twitter_resource=@user.twitter_resources.build(params[:twitter_resource])
+    if @twitter_resource.save!
       flash[:notice]='<p>你说了一些不知道是什么东西的东西。不过你看，我们已经把它加上去了。</p>
 <p>你可以选择回到Michael页面，搜索一下你刚才的胡言乱语试试。</p>
         <p>或者开始另一段胡言乱语.</p>'
       redirect_to user_path(current_user)
-    elsif
-      params[:keywords] =~ %r{[a-zA-z]+://[^\s]*}
+    else
       @known_link_url_resource= Resource.scoped_by_link_url(params[:keywords]).by_owner_value.first
       flash[:link_url]=params[:keywords]
       redirect_to new_user_link_url_resource_path(current_user)
     end
+  end
+
+  private
+  def find_user
+    @user=User.find(params[:user_id])
   end
 end
