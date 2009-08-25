aae1463b5c45ab1ef0286b1f7c00ec42a7d6d3ec
Michael <xxmikl.mexx@gmail.com>
2009-08-25 10:37:33 +0800

    Bug fixed.

--- app/models/news.rb
+++ app/models/news.rb
@@ -2,6 +2,7 @@ class News < ActiveRecord::Base
   belongs_to :owner,:class_name => "User",:foreign_key => "user_id"
   belongs_to :resource
   belongs_to :comment
+  default_scope  :order => "created_at DESC"
 
   def follower
     User.find(self.follower_id)
