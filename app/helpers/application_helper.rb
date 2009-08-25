c2979c18a42a7e53aa53da9870d677ffd73610a6
Michael <xxmikl.mexx@gmail.com>
2009-08-25 11:15:45 +0800

    SEEE

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-    def via_keyword_to_keyword_page(keywords)
+  def via_keyword_to_keyword_page(keywords)
     KeywordPage.find_by_keyword(keywords)
   end
 
   def link_to_follower(id)
     User.find(id)
   end
+
+  
 end
--- app/helpers/news_helper.rb
+++ app/helpers/news_helper.rb
@@ -1,3 +1,2 @@
 module NewsHelper
-
 end
