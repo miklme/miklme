01bbc5f243e82548cdc86a5361dc48fd63d515aa
Michael <xxmikl.mexx@gmail.com>
2009-08-29 11:01:23 +0800

    Going to add the full_text search function.

--- config/environment.rb
+++ config/environment.rb
@@ -3,12 +3,19 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 
-# Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
   # Specify gems that this application depends on and have them installed with rake gems:install
- #config.gem "will_paginate"
+ config.gem "ferret"
+ config.gem "will_paginate"
+ config.gem "locale"
+ config.gem "net-ssh"
+ config.gem "passenger"
+ config.gem "net-ssh-gateway"
+ config.gem "launchy"
+ config.gem "rmagick"
+ config.gem "configuration"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
