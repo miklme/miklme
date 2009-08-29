b2c72e0b40a0fa881fad8975aa193670e978d215
Michael <xxmikl.mexx@gmail.com>
2009-08-29 16:26:57 +0800

    Succeed!!!!!!!!!!(twitter resource search method.)

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -2,30 +2,20 @@ require 'rmmseg'
 require 'rmmseg/ferret'
 
 class Resource < ActiveRecord::Base
-  acts_as_ferret({
-      :fields=>{
-        :keywords=>{
-          :store=>:yes,
-          #:boost => 2
-          #可以设置权重
-        },
-        :content => {
-          :store => :yes
-        },
-        :created_at_s=>{:index=>:untokenized,:store=>:yes},
-        :updated_at_s=>{:index=>:untokenized,:store=>:yes}
-      },
-        :remote => true,
-      #  生产环境下别忘记。
-      :store_class_name=>true,
-      :analyzer=>RMMSeg::Ferret::Analyzer.new
-    })
-  
+
   belongs_to :owner,:class_name => 'User',:foreign_key =>"user_id"
   default_scope :include => :owner,:order => 'users.value DESC'
   named_scope :by_time,:order => "created_at DESC"
   named_scope :in_one_day,:conditions => ["resources.created_at > ?",Time.now.yesterday]
   named_scope :by_owner_value,:include => :owner,:order => 'users.value DESC'
+  
+    def created_at_s
+    created_at.advance(:hours => 8).to_s(:db)
+  end
+  def updated_at_s
+    updated_at.advance(:hours => 8).to_s(:db)
+  end
+
   private
   def adjust_link_url
     if  !self.link_url=~/http:/ and !self.link_url=~/https:/ and  !self.link_url=~/ftp:/ and  !self.link_url=~/sftp:/
@@ -33,10 +23,5 @@ class Resource < ActiveRecord::Base
     end
   end
 
-  def created_at_s
-    created_at.advance(:hours => 8).to_s(:db)
-  end
-  def updated_at_s
-    updated_at.advance(:hours => 8).to_s(:db)
-  end
+
 end
