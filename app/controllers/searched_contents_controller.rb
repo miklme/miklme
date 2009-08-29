1bb028aaebb71c0564a47d8b164ec7cb792d8977
Michael <xxmikl.mexx@gmail.com>
2009-08-29 16:51:18 +0800

    hHOho...twitter_resource result paginate to be finished.

--- app/controllers/searched_contents_controller.rb
+++ app/controllers/searched_contents_controller.rb
@@ -7,7 +7,7 @@ class SearchedContentsController < ApplicationController
 
   def show
     @content=params[:twitter_resource][:content] || ''
-    @twitter_resources=TwitterResource.find_with_ferret(@content)
+    @twitter_resources=TwitterResource.find_with_ferret(@content,:per_page => 2,:page => params[:page])
   end
 
 end
