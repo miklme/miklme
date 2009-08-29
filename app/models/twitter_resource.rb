e8ac3bf1739acc75c6e508a0e7083daabccbab37
Michael <xxmikl.mexx@gmail.com>
2009-08-29 16:21:44 +0800

    Seccessing...The problem is I didn't start the ferret server.

--- app/models/twitter_resource.rb
+++ app/models/twitter_resource.rb
@@ -1,9 +1,24 @@
 class TwitterResource<Resource
+  acts_as_ferret({
+      :fields=>{
+        :content => {
+          :store => :yes
+        },
+        :created_at_s=>{:index=>:untokenized,:store=>:yes},
+        :updated_at_s=>{:index=>:untokenized,:store=>:yes}
+      },
+      #  :remote => true,
+      #  生产环境下别忘记。
+      :store_class_name=>true,
+      :analyzer=>RMMSeg::Ferret::Analyzer.new
+    })
+  
   has_many :replies,:foreign_key => "resource_id"
   has_many :repliers,:through => :replies,:source => :user
 
   validates_length_of :content,:maximum => 130,:message => "内容有点长了。请保持在130字以内。"
   validates_presence_of :content,:message => "请填写完全"
+
 
   def self.search_by_content(content,page)
     paginate :per_page => 15,
