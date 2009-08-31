e5bc3fe244c24749f3414550da11e3ba9d59afbf
Michael <xxmikl.mexx@gmail.com>
2009-08-31 20:49:44 +0800

    Fixed another bug.

--- app/controllers/searched_keywords_controller.rb
+++ app/controllers/searched_keywords_controller.rb
@@ -3,15 +3,6 @@ class SearchedKeywordsController < ApplicationController
 
   end
 
-  def notification
-    #    re=Regexp.new("^#{params[:resource][:keywords]}","i")
-    #    @resources=Resource.all do |r|
-    #      r.keywords.to_s.match re
-    #    end
-    #    render :update do |page|
-    #      page.replace_html  "notification","当您幸运的发现关键字没有出现在列表中时，就意味着您可以任意编辑该关键字的内容，从而被别人看见。"
-    #    end
-  end
 
   def auto_complete_for_link_url_resource_keywords
     link_url_resources=LinkUrlResource.find_with_ferret(params[:link_url_resource][:keywords])
