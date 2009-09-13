f6e5249e501fe7cce3abf966e0bdfb834aaa6a77
Michael <xxmikl.mexx@gmail.com>
2009-09-13 15:49:55 +0800

    a little bug.

--- app/models/blog_resource.rb
+++ app/models/blog_resource.rb
@@ -16,7 +16,7 @@ class BlogResource<Resource
   has_many :comments,:foreign_key => "resource_id"
   has_many :commenters,:through => :comments,:source => :user
 
-  validates_length_of :keywords,:within => 1..20
+  validates_length_of :keywords,:within => 1..50
   validates_length_of :content,:minimum => 150
   validates_length_of :title,:within => 1..30
 end
