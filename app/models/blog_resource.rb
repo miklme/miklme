2ede905547e548fdf02f4dad2766c22574ad16cc
Michael <xxmikl.mexx@gmail.com>
2009-09-25 17:26:53 +0800

    bug

--- app/models/blog_resource.rb
+++ app/models/blog_resource.rb
@@ -24,5 +24,5 @@ class BlogResource<Resource
   has_many :commenters,:through => :comments,:source => :owner
 
   validates_length_of :content,:minimum => 120
-  validates_length_of :title,:within => 1..30
+  validates_length_of :title,:within => 1..60
 end
--- app/models/link_url_resource.rb
+++ app/models/link_url_resource.rb
@@ -18,7 +18,7 @@ class LinkUrlResource< Resource
   has_many :comments,:foreign_key => "resource_id"
   has_many :commenters,:through => :comments,:source => :owner
   validates_format_of :link_url, :with => %r{[a-zA-z]+://[^\s]*},:message => "看看是否包含了http://"
-  validates_length_of :description,:within => 1..30
+  validates_length_of :description,:within => 1..60
   validates_length_of :link_url,:maximum => 1000
   named_scope :hot_resources,:include => :comments,:condition => "comments.size>10"
 
