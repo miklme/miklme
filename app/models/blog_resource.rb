4bcb865b433c91ec1d961a0c1e61d3fba8116503
Michael <xxmikl.mexx@gmail.com>
2009-09-28 14:32:53 +0800

    Signed-off-by: Michael <xxmikl.mexx@gmail.com>

--- app/models/blog_resource.rb
+++ app/models/blog_resource.rb
@@ -23,6 +23,6 @@ class BlogResource<Resource
   has_many :comments,:foreign_key => "resource_id",:dependent => :destroy
   has_many :commenters,:through => :comments,:source => :owner
 
-  validates_length_of :content,:minimum => 120
+  validates_length_of :content,:minimum => 2
   validates_length_of :title,:within => 1..60
 end
