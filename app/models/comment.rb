d5dfdd57dea21f294c2b74152459c345b0799ab5
Michael <xxmikl.mexx@gmail.com>
2009-09-30 15:29:39 +0800

    A little bug fixed.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,7 +7,7 @@ class Comment < ActiveRecord::Base
 
   named_scope :by_owner_value, :include => :owner,:order => 'users.total_value DESC'
   named_scope :by_time,:order => "created_at DESC"
-  named_scope :recent_comments,:limit => 3,:order => 'created_at DESC'
+  named_scope :recent_comments,:limit => 3,:order => 'comments.created_at DESC'
   named_scope :parent_comments,:conditions => "parent_comment_id is NULL"
   validates_presence_of :content
 
