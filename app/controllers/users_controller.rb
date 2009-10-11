5d05e9a66c1bfeb8168f77ffc93b63f2bd34b12e
Michael <xxmikl.mexx@gmail.com>
2009-10-11 22:20:15 +0800

    sdf

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -63,7 +63,7 @@ class UsersController < ApplicationController
     @user=User.find(params[:id])
     @news=News.list_self_news(@user,params[:page])
     @self_news_for_others=News.self_news_for_others(@user,params[:page])
-    @variable_title="我的新鲜事"
+    @variable_title="回复我的"
     @resources=@user.resources.find(:all,:order => "resources.created_at DESC")
     render :layout => "resources"
   end
