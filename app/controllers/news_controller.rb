3317ec3622babbdedca39e594218641e16129354
Michael <xxmikl.mexx@gmail.com>
2009-10-21 13:23:20 +0800

    ddddd

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -6,7 +6,11 @@ class NewsController < ApplicationController
     @followers=@user.followers
   end
 
-  def destroy
-    
+  def show
+    keyword_page_news=News.find(params[:id])
+    redirect_to user_resource_comments_path(keyword_page_news.resource.owner,keyword_page_news.resource)
+    if keyword_page_news.owner==current_user
+      keyword_page_news.destroy
+    end
   end
 end
